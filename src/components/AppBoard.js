eccc798b564bdf23d5c5934570f1e58332da4fe8
tintinnkt <t.tintinnkt@gmail.com>
2024-12-05 00:20:34 +0700

    refactor : refactor get class and name for cell to mapping

--- src/components/AppBoard.js
+++ src/components/AppBoard.js
@@ -1,62 +1,43 @@
 import React from "react";
-import "./AppBoard.css";
+import "./_css/AppBoard.css";
 
-const AppBoard = (props) => {
-    const { board } = props;
-    const getClassForCell = (cell) => {
-        switch (cell) {
-            case "Px1":
-                return "gray-cell";
-            case "Px2":
-                return "orange-cell";
-            case "Px3":
-                return "blue-cell";
-            case "Ex2":
-                return "yellow-cell";
-            case "Ex3":
-                return "red-cell";
-            default:
-                return "";
-        }
-    };
-
-    const getNameForCell = (name) => {
-        switch (name) {
-            case "Px1":
-                return "";
-            case "Px2":
-                return "2xP";
-            case "Px3":
-                return "3xP";
-            case "Ex2":
-                return "2xE";
-            case "Ex3":
-                return "3xE";
-            default:
-                return "";
-        }
-    };
-
-    return (
-      <div className="board">
-        {board.map((row, rowIndex) => (
-          <div className="row" key={rowIndex}>
-            {row.map((cell, cellIndex) => (
-              <div
-                className={`cell ${getClassForCell(cell)}`}
-                key={`${rowIndex}-${cellIndex}`}
-              >
-                {cell === "str" ? (
-                  <img src="./star-image.png" alt="Star" className="str-image" />
-                ) : (
-                  getNameForCell(cell)
-                )}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-    );
+export default function AppBoard(props) {
+  const { board } = props;
+  return (
+    <main className="board">
+      {board.map((row, rowIndex) => (
+        <div className="row" key={rowIndex}>
+          {row.map((cell, cellIndex) => (
+            <div
+              className={`cell ${getClassForCell(cell)}`}
+              key={`${rowIndex}-${cellIndex}`}
+            >
+              {cell === "str" ? (
+                <img src="./star-image.png" alt="Star" className="str-image" />
+              ) : (getNameForCell(cell))}
+            </div>
+          ))}
+        </div>
+      ))}
+    </main>
+  );
 };
 
-export default AppBoard;
+const classMapping = {
+  Px1: "gray-cell",
+  Px2: "orange-cell",
+  Px3: "blue-cell",
+  Ex2: "yellow-cell",
+  Ex3: "red-cell",
+};
+
+const nameMapping = {
+  Px1: "",
+  Px2: "2xP",
+  Px3: "3xP",
+  Ex2: "2xE",
+  Ex3: "3xE",
+};
+
+const getClassForCell = (cell) => classMapping[cell] || "";
+const getNameForCell = (name) => nameMapping[name] || "";
